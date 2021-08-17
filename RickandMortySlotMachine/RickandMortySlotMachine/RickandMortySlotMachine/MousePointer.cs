446db838674f0c32470e0dde7d5f18ea855b8a6a
RaghavPu <46982251+RaghavPu@users.noreply.github.com>
2021-08-17 11:23:44 -0500

    Add files via upload

---
+++ RickandMortySlotMachine/RickandMortySlotMachine/RickandMortySlotMachine/MousePointer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace RickandMortySlotMachine
+{
+    public class MousePointer
+    {
+        public static Texture2D circleImage;
+        public static int POINTER_SIZE = 30;
+        public static double MOUSE_SENSITIVITY = 4.5;
+        public Vector2 screenSize;
+
+        public double x, y;
+        public Rectangle container;
+
+        public MousePointer(double x, double y, int width, int height)
+        {
+            this.x = x;
+            this.y = y;
+            this.container = new Rectangle((int)x, (int)y, POINTER_SIZE, POINTER_SIZE);
+            this.screenSize = new Vector2(width, height);
+        }
+
+        public static void loadPointerImage(Microsoft.Xna.Framework.Game game)
+        {
+            circleImage = game.Content.Load<Texture2D>("CirclePointer");
+        }
+
+        public void Update(MouseState mouse)
+        {
+            this.container.X = mouse.X;
+            this.container.Y = mouse.Y;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(circleImage, container, null, Color.White, 0, new Vector2(circleImage.Width / 2, circleImage.Height / 2), SpriteEffects.None, 0);
+        }
+    }
+}
---
+++ RickandMortySlotMachine/RickandMortySlotMachine/RickandMortySlotMachine/StartScreen.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace RickandMortySlotMachine
+{
+    class StartScreen
+    {
+        MousePointer mousePointer;
+
+        public StartScreen(int width, int height)
+        {
+            mousePointer = new MousePointer(50, 50, width, height);
+        }
+
+        public void Update(MouseState mouseState)
+        {
+            mousePointer.Update(mouseState);
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            mousePointer.Draw(spriteBatch);
+        }
+    }
+}
